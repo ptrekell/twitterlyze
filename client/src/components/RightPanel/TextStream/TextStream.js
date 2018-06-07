ba10e2d022e0c7a031ee88d44491d52a78ebcd00
P Trekell <pavel.trekell@gmail.com>
2018-06-07 14:46:57 -0700

    socket io working

--- client/src/components/RightPanel/TextStream/TextStream.js
+++ client/src/components/RightPanel/TextStream/TextStream.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import './TextStream.css';
+
+
 
 class TextStream extends Component {
 
     render() {
+
+        const socket = io.connect("/");
+
+        socket.on("hello",function(num){
+            console.log("hi",num);
+          })
+
+
         return (
             <div className="TextStream">
                 TextStream message: {this.props.message}
