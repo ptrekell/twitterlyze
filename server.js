4b49348bacc882a7f20727977610c09b194cbaa7
P Trekell <pavel.trekell@gmail.com>
2018-06-07 07:10:03 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const reload = require('reload')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express peeps there!' });
+});
+
+
+reload(app);
+app.listen(port, () => console.log(`Listening on port ${port}`));
