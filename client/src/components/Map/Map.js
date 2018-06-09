01ef820fed3ca8ddd94d7cc5ff5c5109f1a458dc
P Trekell <pavel.trekell@gmail.com>
2018-06-09 16:44:05 -0700

    addedd react js google maps

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,16 +1,89 @@
 import React, { Component } from 'react';
+import { compose, withProps, withHandlers } from 'recompose';
+
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer';
+
+import mapStyle from './mapStyle.json';
+import twitterIcon from './twitterIcon.svg';
+
+
+
 import './Map.css';
+
+
+
+const MapWithAMarkerClusterer = compose(
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDUQppaMAmd54CmBNqX2iBeXHQl4uasQ6s&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `100%` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withHandlers({
+        onMarkerClustererClick: () => (markerClusterer) => {
+            const clickedMarkers = markerClusterer.getMarkers()
+            console.log(`Current clicked markers length: ${clickedMarkers.length}`)
+            console.log(clickedMarkers)
+        },
+    }),
+    withScriptjs,
+    withGoogleMap
+)(props =>
+    <GoogleMap
+        defaultZoom={4}
+        defaultCenter={{ lat: 40.5692, lng: -98.3321 }}
+        defaultOptions={{ styles: mapStyle }}
+    >
+        <MarkerClusterer
+            onClick={props.onMarkerClustererClick}
+            averageCenter
+            enableRetinaIcons
+            gridSize={60}
+        >
+            {props.markers.map(marker => (
+                <Marker
+                    key={marker.photo_id}
+                    position={{ lat: marker.latitude, lng: marker.longitude }}
+                    label={marker.label}  
+                    icon={twitterIcon}               
+                />
+            ))}
+        </MarkerClusterer>
+    </GoogleMap>
+);
+
+
+
 
 class Map extends Component {
 
-    render() {
+    state = {
+        markers: [
+            {
+                "photo_id": 27932,
+                "longitude": -122.3321 ,
+                "latitude": 46.662,
+                "label": ""
+ 
+            },
+            // {
+            //     "photo_id": 522084,
+            //     "longitude": -122.3321 ,
+            //     "latitude": 47.662,
+            //     "label": ""
+            // }
+        ]
+    }
 
+    render() {
         return (
             <div className="Map">
-                Hi from maps
+                <MapWithAMarkerClusterer markers={this.state.markers} />
             </div>
         )
     }
+
 
 }
 
--- client/src/components/RightPanel/SearchBar/SearchBar.js
+++ client/src/components/RightPanel/SearchBar/SearchBar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
 
+
 class SearchBar extends Component {
 
     render() {
         return (
             <div className="SearchBar">
-                <input class="form-control form-control-lg" style={{color:"black"}} type="text" placeholder="search..." />
+                <input className="form-control form-control-lg" type="text" placeholder="search..." />
+
             </div>
            
         )
